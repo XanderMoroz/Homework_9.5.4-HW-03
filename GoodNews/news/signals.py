f8507e8b7d743dc0847af93eca7a66f81d57c109
Александр <7176uiop3436>
2022-06-24 20:01:20 +0300

    Functions: notify_subscribers, check_post_limits added

--- GoodNews/news/signals.py
+++ GoodNews/news/signals.py
@@ -1,4 +1,6 @@
-from django.db.models.signals import post_save, m2m_changed
+from datetime import datetime
+
+from django.db.models.signals import post_save, m2m_changed, pre_save
 from django.dispatch import receiver
 from django.core.mail import mail_managers, EmailMultiAlternatives
 from django.template.loader import render_to_string
@@ -15,11 +17,16 @@ def notify_subscribers(sender, instance, action, **kwargs):
         sendto_set = set()
         # формируем список для рассылки
         for cat in cats:
-            sendto_set |= cat.get_subscribers_emails()
+            sendto_set = cat.get_subscribers_emails()
         msg = EmailMultiAlternatives(
-            subject=f'{instance.post_title}',
+            subject=f'"Здравствуйте, {instance.author.user} Новая статья в твоём любимом разделе!"',
             body=f'{instance.post_text}',
             from_email=settings.DEFAULT_FROM_EMAIL,
             to=sendto_set)
         msg.attach_alternative(html_content, "text/html")
         msg.send()
+
+@receiver(pre_save, sender=Post)
+def check_post_limits(sender, instance, **kwargs):
+    today_posts = Post.objects.filter(time_in__date=datetime.datetime.now().date())
+    return len(today_posts)
